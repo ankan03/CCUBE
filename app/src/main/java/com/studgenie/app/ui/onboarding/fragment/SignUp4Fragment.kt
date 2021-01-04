3a43ead69e795ddc918e05e1f643f6f56f84af6f
ankan03 <1828049@kiit.ac.in>
2021-01-04 12:10:21 +0530

    Back button functionality added in 4th screen

--- app/src/main/java/com/studgenie/app/ui/onboarding/fragment/SignUp4Fragment.kt
+++ app/src/main/java/com/studgenie/app/ui/onboarding/fragment/SignUp4Fragment.kt
@@ -11,6 +11,7 @@ import androidx.fragment.app.Fragment
 import com.studgenie.app.R
 import com.studgenie.app.ui.main.activity.HomeActivity
 
+@Suppress("DEPRECATION")
 class SignUp4Fragment : Fragment(){
 
     override fun onCreateView(
@@ -20,22 +21,19 @@ class SignUp4Fragment : Fragment(){
     ): View? {
         val rootView = inflater.inflate(R.layout.fragment_sign_up_4, container, false)
         val beginLearning = rootView.findViewById<Button>(R.id.verify_proceed_button)
+        val backArrow = rootView.findViewById<Button>(R.id.back_arrow_button)
+
         beginLearning.setOnClickListener {
             val i = Intent(activity, HomeActivity::class.java)
-                                                startActivity(i)
-                                                (activity as Activity?)!!.overridePendingTransition(
-                                                    0,
-                                                    0
-                                                )
+            startActivity(i)
+            (activity as Activity?)!!.overridePendingTransition(0, 0)
+            activity?.finish()
+        }
+        backArrow.setOnClickListener {
+            val signUp3Fragment = SignUp3Fragment()
+            fragmentManager!!.beginTransaction().replace(R.id.signup_fragment_container,signUp3Fragment).commit()
         }
         return rootView
 
     }
 }
-
-//val i = Intent(activity, HomeActivity::class.java)
-//                                                startActivity(i)
-//                                                (activity as Activity?)!!.overridePendingTransition(
-//                                                    0,
-//                                                    0
-//                                                )
